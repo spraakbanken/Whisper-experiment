99d73c789d7fbeb4ebef810d6f6e7f8a494bc8b7
Herbert Lange <herbert.lange@gu.se>
2025-06-27 14:48:27 +0200

    shared model parameters and support for temperature

--- transcribe.py
+++ transcribe.py
@@ -7,33 +7,36 @@ import time
 
 languages = ['sv', '']
 condition_on_previous = True
-model_sizes = [ "small", "large" ]
+#model_sizes = [ "small", "large" ]
+model_sizes = [ "small"]
 audio_files = sys.argv[1:]
+temperatures = [0,0.25,0.5,0.75,1]
+model_parameters = {
+        "device" : "cuda",
+        # device = "cpu",
+        # compute_type="float16",
+        "compute_type" : "float32",
+        "download_root" : "cache",
+        # condition_on_previous_text = False # Can reduce hallucinations if we don't use prompts
+}
 
 models = {}
 models["stable_ts"] = {}
 models["openai"] = {}
 models["kb_whisper"] = {}
 for model_size in model_sizes:
-    models["stable_ts"][model_size] = stable_whisper.load_faster_whisper(model_size)
+    models["stable_ts"][model_size] = stable_whisper.load_faster_whisper(
+        model_size,
+        **model_parameters
+    )
 
     models["openai"][model_size] = WhisperModel(
         model_size,
-        # device="cuda",
-        device = "cpu",
-        # compute_type="float16",
-        compute_type="float32",
-        download_root="cache", # cache directory
-        # condition_on_previous_text = False # Can reduce hallucinations if we don't use prompts
+        **model_parameters
     )
     models["kb_whisper"][model_size] = WhisperModel(
         "KBLab/kb-whisper-" + model_size,
-        # device="cuda",
-        device = "cpu",
-        # compute_type="float16",
-        compute_type="float32",
-        download_root="cache", # cache directory
-        # condition_on_previous_text = False # Can reduce hallucinations if we don't use prompts
+        **model_parameters
     )
 # if len(sys.argv) > 2:
 #     generate_kwargs["language"] = sys.argv[2]
@@ -42,56 +45,56 @@ for audio_file in audio_files:
     for m in models.keys():
         for model_size in model_sizes:
             for language in languages:
-                kwargs = {}
-                if language:
-                    kwargs["language"] = language
-                    file_name = "_".join([os.path.splitext(audio_file)[0], "whisper", m, model_size, language])
-                else:
-                    file_name = "_".join([os.path.splitext(audio_file)[0], "whisper", m, model_size])
-                text_file_name = file_name + ".txt"
-                segments_file_name = file_name + ".segments"
-                if not Path(text_file_name).exists():
-                    print(text_file_name)
-                    if m == "stable_ts":
-                        start = time.process_time()
-                        result = models[m][model_size].transcribe(audio_file, condition_on_previous_text=condition_on_previous, **kwargs)
-                        end = time.process_time()
-                        print("Took " +str(end-start) + " s to transcribe " + text_file_name)
-                        text = result.text
+                for temperature in temperatures:
+                    kwargs = {"temperature": temperature}
+                    if language:
+                        kwargs["language"] = language
+                        file_name = "_".join([os.path.splitext(audio_file)[0], "whisper", m, model_size, language, str(temperature)])
                     else:
-                        start = time.process_time()
-                        segments, info = models[m][model_size].transcribe(audio_file, condition_on_previous_text=condition_on_previous, log_progress=True, **kwargs)
-                        end = time.process_time()
-                        print("Took " +str(end-start) + " s to transcribe " + text_file_name)
-                        print("Detected Language: " + info.language)
-                        text = ' '.join([segment.text for segment in segments])
-                    with open(text_file_name, 'w') as f:
-                        f.write(text)
-                if not Path(segments_file_name).exists():
-                    print(segments_file_name)
-                    lines = []
-                    index = 1
-                    if m == "stable_ts":
-                        start = time.process_time()
-                        result = models[m][model_size].transcribe(audio_file, condition_on_previous_text=condition_on_previous, **kwargs)
-                        end = time.process_time()
-                        print("Took " +str(end-start) + " s to transcribe " + segments_file_name)
-                        for segment in result.segments:
-                            lines.append("\t".join([str(index),str(segment.start),str(segment.end),segment.text]))
-                            index+=1
-                    else:
-                        start = time.process_time()
-                        segments, info = models[m][model_size].transcribe(audio_file, condition_on_previous_text=condition_on_previous, log_progress=True, **kwargs)
-                        end = time.process_time()
-                        print("Took " +str(end-start) + " s to transcribe " + segments_file_name)
-                        for segment in segments:
-                            lines.append("\t".join([str(index),str(segment.start),str(segment.end),segment.text]))
-                            index+=1
-
+                        file_name = "_".join([os.path.splitext(audio_file)[0], "whisper", m, model_size, "auto", str(temperature)])
+                    text_file_name = file_name + ".txt"
+                    segments_file_name = file_name + ".segments"
+                    if not Path(text_file_name).exists():
+                        print(text_file_name)
+                        if m == "stable_ts":
+                            start = time.process_time()
+                            result = models[m][model_size].transcribe(audio_file, condition_on_previous_text=condition_on_previous, **kwargs)
+                            end = time.process_time()
+                            print("Took " +str(end-start) + " s to transcribe " + text_file_name)
+                            text = result.text
+                        else:
+                            start = time.process_time()
+                            segments, info = models[m][model_size].transcribe(audio_file, condition_on_previous_text=condition_on_previous, log_progress=True, **kwargs)
+                            end = time.process_time()
+                            print("Took " +str(end-start) + " s to transcribe " + text_file_name)
+                            print("Detected Language: " + info.language)
+                            text = ' '.join([segment.text for segment in segments])
+                        with open(text_file_name, 'w') as f:
+                            f.write(text)
+                    if not Path(segments_file_name).exists():
+                        print(segments_file_name)
+                        lines = []
+                        index = 1
+                        if m == "stable_ts":
+                            start = time.process_time()
+                            result = models[m][model_size].transcribe(audio_file, condition_on_previous_text=condition_on_previous, **kwargs)
+                            end = time.process_time()
+                            print("Took " +str(end-start) + " s to transcribe " + segments_file_name)
+                            for segment in result.segments:
+                                lines.append("\t".join([str(index),str(segment.start),str(segment.end),segment.text]))
+                                index+=1
+                        else:
+                            start = time.process_time()
+                            segments, info = models[m][model_size].transcribe(audio_file, condition_on_previous_text=condition_on_previous, log_progress=True, **kwargs)
+                            end = time.process_time()
+                            print("Took " +str(end-start) + " s to transcribe " + segments_file_name)
+                            for segment in segments:
+                                lines.append("\t".join([str(index),str(segment.start),str(segment.end),segment.text]))
+                                index+=1
 #                        text = ' '.join([segment.text for segment in segments])
-                    with open(segments_file_name, 'w') as f:
-                        text = "\n".join(lines)
-                        f.write(text)
+                        with open(segments_file_name, 'w') as f:
+                            text = "\n".join(lines)
+                            f.write(text)
 # print("Detected language '%s' with probability %f" % (info.language, info.language_probability))
 
 # #print(segments)
